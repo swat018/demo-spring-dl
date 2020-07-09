63c27c8c1da55fdf34dc2096ad6f09731720d985
jinwoo park <swat018@gmail.com>
2020-07-10 07:42:36 +0900

    Initial commit

---
+++ src/main/java/com/swat018/demospringdl/DemoSpringDlApplication.java
@@ -0,0 +1,13 @@
+package com.swat018.demospringdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringDlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringDlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swat018/demospringdl/DemoSpringDlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swat018.demospringdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringDlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
