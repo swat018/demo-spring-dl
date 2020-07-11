076d80fa429834ab215f719670061a8d93649d0e
jinwoo park <swat018@gmail.com>
2020-07-11 17:26:29 +0900

    다아나믹 프록시 - 스프링 데이터 JPA는 어떻게 동작하나?

--- src/main/java/com/swat018/demospringdl/BookRepository.java
+++ src/main/java/com/swat018/demospringdl/BookRepository.java
@@ -1,7 +1,11 @@
 package com.swat018.demospringdl;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+/*@Repository
 public class BookRepository {
+}*/
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
 }
---
+++ src/test/java/com/swat018/demospringdl/BookRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.swat018.demospringdl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class BookRepositoryTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    public void di() {
+        assertNotNull(bookRepository);
+
+        Book book = new Book();
+        book.setTitle("spring");
+        bookRepository.save(book);
+
+        bookRepository.findAll();
+    }
+}
